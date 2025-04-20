9c51b1c4604aa1f73fa58ddd26730d327c9fc551
jerecito <jeregiu7@gmail.com>
2025-04-20 18:31:55 -0300

    esta todo bien hasta el punto 2

--- desafio1.js
+++ desafio1.js
@@ -3,18 +3,28 @@ const seedrandom = require('seedrandom');
 function generar_num(){
     const semilla = seedrandom(1763519);
     const num_random = [];
-    for (let i = 0; i < 100000; i++){num_random.push(semilla.int32())}
+    for (let i = 0; i < 1000000; i++){num_random.push(semilla.int32())}
     return num_random
 }
 
-///punto 1
+// punto 1
 const numerosAleatorios = generar_num()
 let positivos = 0
 let negativos = 0
-for ( let i of num_random){
-    if (i > 0){ positivos++};
+for ( let i of numerosAleatorios){
+    if (i > 0){positivos++};
     if (i < 0){negativos ++} 
 }
 
-console.log('La cantidad de positivos es:  ${positivos}');
-console.log('la cantidad de negativos es: ${negativos}')
+console.log(`la cantidad de positivos es: ${positivos}`);
+console.log(`la cantidad de positivos es: ${negativos}`);
+
+// punto 2
+
+let contador = 0
+for (let i of numerosAleatorios){
+    if ((i % 7 === 0) || (i % 7 === 3) || (i % 7 === 5) || (i % 7 === 6)){contador++};
+}
+console.log(`la cantidad de números con esas caracteristicas son: ${contador}`)
+
+// punto 3
